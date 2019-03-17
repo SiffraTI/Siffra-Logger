fdc7efc1da82b3cd3f93071fdc2c0c166d3d2f4d
Luiz Benevenuto <luiz@siffra.com.br>
2019-03-17 02:18:36 -0300

    :pencil: Small changes

--- lib/Siffra/Logger.pm
+++ lib/Siffra/Logger.pm
@@ -3,134 +3,112 @@ package Siffra::Logger;
 use 5.014;
 use strict;
 use warnings;
-use Carp;
-0 = 1;
 use utf8;
-use Data::Dumper;
-use Log::Any qw($log);
-use Scalar::Util qw(blessed);
- 
-BEGIN {
+
+$| = 1;    #autoflush
+
+BEGIN
+{
     use Exporter ();
     use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
-    $VERSION     = '0.01';
-    @ISA         = qw(Exporter);
+    $VERSION = '0.01';
+    @ISA     = qw(Exporter);
+
     #Give a hoot don't pollute, do not export more than needed by default
     @EXPORT      = qw();
     @EXPORT_OK   = qw();
     %EXPORT_TAGS = ();
-}
- 
+} ## end BEGIN
 
-#################### subroutine header begin ####################
-
-=head2 sample_function
-
- Usage     : How to use this function/method
- Purpose   : What it does
- Returns   : What it returns
- Argument  : What it wants to know
- Throws    : Exceptions and other anomolies
- Comment   : This is a sample subroutine header.
-           : It is polite to include more pod and fewer comments.
-
-See Also   :
-
-=cut
-
-#################### subroutine header end ####################
-
-
-=head2 C<new()>
- 
-  Usage     : $self->block_new_method() within text_pm_file()
-  Purpose   : Build 'new()' method as part of a pm file
-  Returns   : String holding sub new.
-  Argument  : $module: pointer to the module being built
-              (as there can be more than one module built by EU::MM);
-              for the primary module it is a pointer to $self
-  Throws    : n/a
-  Comment   : This method is a likely candidate for alteration in a subclass,
-              e.g., pass a single hash-ref to new() instead of a list of
-              parameters.
- 
-=cut
- 
-sub new
+BEGIN
 {
-    my ($class, %parameters) = @_;
+    use Log::Any::Adapter;
+    use Log::Dispatch;
+    use File::Basename;
+    use Data::Dumper;
+    use POSIX qw/strftime/;
 
-    my $self = {};
- 
-    $self = bless ($self, ref ($class) || $class);
+    binmode( STDOUT, ":encoding(UTF-8)" );
+    binmode( STDERR, ":encoding(UTF-8)" );
+    $ENV{ LC_ALL } = $ENV{ LANG } = 'pt_BR.UTF-8';
 
-    $log->info( "new", { progname => $0, pid => $$, perl_version => $], package => __PACKAGE__ } );
- 
-    $self->_initialize( %parameters );
-    return $self;
-}
- 
-sub _initialize()
-{
-    my ( $self, %parameters ) = @_;
-    $log->info( "_initialize", { package => __PACKAGE__ } );
-} ## end sub _initialize
+    my ( $filename, $baseDirectory, $suffix ) = fileparse( $0, qr/\.[^.]*/ );
+    my $logDirectory = $baseDirectory . 'logs/';
+    my $logFilename  = $filename . '.log';
+    croak( "Unable to create $logDirectory" ) unless ( -e $logDirectory or mkdir $logDirectory );
 
-sub END
-{
-    $log->info( "END", { package => __PACKAGE__ } );
-}
+    my $dispatcher = Log::Dispatch->new(
+        outputs => [
+            [
+                'Screen',
+                name      => 'screen',
+                min_level => 'debug',
+                max_level => 'warning',
+                newline   => 1,
+                utf8      => 0,
+                stderr    => 0,
+                use_color => 1,
+            ],
+            [
+                'Screen',
+                name      => 'screen-error',
+                min_level => 'error',
+                newline   => 1,
+                utf8      => 0,
+                stderr    => 1,
+                use_color => 1,
+            ],
+            [
+                'File',
+                name      => 'file-01',
+                filename  => $logDirectory . $logFilename,
+                min_level => 'debug',
+                newline   => 1,
+                mode      => 'write',
+                binmode   => ':encoding(UTF-8)',
+            ]
+        ],
+        callbacks => [
+            sub {
+                my %msg = @_;
+                my $i   = 0;
+                my @array_caller;
+                my ( $package, $filename, $line, $subroutine, $hasargs, $wantarray, $evaltext, $is_require, $hints, $bitmask, $hinthash );
 
-=head2 C<AUTOLOAD>
- 
-  Usage     : $self->block_new_method() within text_pm_file()
-  Purpose   : Build 'new()' method as part of a pm file
-  Returns   : String holding sub new.
-  Argument  : $module: pointer to the module being built
-              (as there can be more than one module built by EU::MM);
-              for the primary module it is a pointer to $self
-  Throws    : n/a
-  Comment   : This method is a likely candidate for alteration in a subclass,
-              e.g., pass a single hash-ref to new() instead of a list of
-              parameters.
- 
-=cut
+                do
+                {
+                    ( $package, $filename, $line, $subroutine, $hasargs, $wantarray, $evaltext, $is_require, $hints, $bitmask, $hinthash ) = caller( $i++ );
 
-sub AUTOLOAD
-{
-    my ( $self, @parameters ) = @_;
-    our $AUTOLOAD;
+                    my $auxiliar = {
+                        package    => $package,
+                        filename   => $filename,
+                        line       => $line,
+                        subroutine => $subroutine,
+                    };
 
-    return if ( $AUTOLOAD =~ /DESTROY/ );
-} ## end sub AUTOLOAD
+                    push( @array_caller, $auxiliar );
+                } until ( !defined $line or $line == 0 or $subroutine =~ /(eval)/ );
 
-sub DESTROY
-{
-    my ( $self, %parameters ) = @_;
-    $log->info( 'DESTROY', { package => __PACKAGE__, GLOBAL_PHASE => ${^GLOBAL_PHASE} } );
-    return if ${^GLOBAL_PHASE} eq 'DESTRUCT';
+                my $mensage = sprintf( "%s [ %9.9s ] [ pid: %d ] - %s - [ %s ]", strftime( "%F %H:%M:%S", localtime ), uc( $msg{ level } ), $$, $msg{ message }, $array_caller[ -2 ]->{ subroutine } );
 
-    if ( blessed( $self ) && $self->isa( __PACKAGE__ ) )
-    {
-        $log->alert( "DESTROY", { package => __PACKAGE__, GLOBAL_PHASE => ${^GLOBAL_PHASE}, blessed => 1 } );
-    }
-    else
-    {
-        # TODO
-    }
-} ## end sub DESTROY
- 
+                return $mensage;
+            }
+        ]
+    );
+
+    Log::Any::Adapter->set( 'Dispatch', dispatcher => $dispatcher );
+} ## end BEGIN
+
 #################### main pod documentation begin ###################
 ## Below is the stub of documentation for your module.
 ## You better edit it!
- 
 
 =encoding UTF-8
 
 
 =head1 NAME
 
-Siffra::Logger - Module abstract (<= 44 characters) goes here
+Siffra::Logger - Siffra config for Log::Any
 
 =head1 SYNOPSIS
 
@@ -181,10 +159,10 @@ LICENSE file included with this module.
 perl(1).
  
 =cut
- 
+
 #################### main pod documentation end ###################
- 
 
 1;
+
 # The preceding line will help the module return a true value
 
--- t/001_load.t
+++ t/001_load.t
@@ -2,11 +2,6 @@
 
 # t/001_load.t - check module loading and create testing directory
 
-use Test::More tests => 2;
+use Test::More tests => 1;
 
 BEGIN { use_ok( 'Siffra::Logger' ); }
-
-my $object = Siffra::Logger->new ();
-isa_ok ($object, 'Siffra::Logger');
-
-
